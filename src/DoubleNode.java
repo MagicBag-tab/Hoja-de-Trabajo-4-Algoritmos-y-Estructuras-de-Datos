f79e7b45881403cf7298d37a58c32ba9ef6c6206
MagicBag-tab <est24347@uvg.edu.gt>
2025-02-27 20:04:29 -0600

    Implementacion de los nodos y sus metodos para las listas doblemente encadenadas

--- src/DoubleNode.java
+++ src/DoubleNode.java
@@ -1,3 +1,35 @@
-public class DoubleNode {
+public class DoubleNode<T> {
+
+    DoubleNode<T> next = null;
+    DoubleNode<T> previous = null;
+    T value;
+
+    public DoubleNode(T value) {
+        this.value = value;
+    }
+
+    public DoubleNode() {
+
+    }
+
+    public DoubleNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(DoubleNode<T> next) {
+        this.next = next;
+    }
+
+    public DoubleNode<T> getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(DoubleNode<T> previous) {
+        this.previous = previous;
+    }
+
+    public T getValue() {
+        return value;
+    }
 
 }
