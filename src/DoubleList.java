0cc16c2dbb0a83e0c743abcd513a378afb7a47e2
MagicBag-tab <est24347@uvg.edu.gt>
2025-02-27 21:13:04 -0600

    Se implementaron los metodos de add y delete en el doublelist modificando el codigo hecho para simple list

--- src/DoubleList.java
+++ src/DoubleList.java
@@ -1,3 +1,54 @@
-public class DoubleList {
+public class DoubleList<T> implements IList<T> {
+
+    private DoubleNode<T> first;
+    private DoubleNode<T> last;
+    private int count = 0;
+
+    @Override
+    public void add(T value) {
+        DoubleNode<T> nuevo = new DoubleNode<>(value);
+        if (first == null) {
+            first = nuevo;
+            last = nuevo;
+        } else {
+            last.setNext(nuevo);
+            nuevo.setPrevious(last);
+            last = nuevo;
+        }
+
+        count++;
+    }
+
+    @Override
+    public void delete(int index) {
+        if (index < 0 || index >= count) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango");
+        }
+
+        if (index == 0) {
+            first = first.getNext();
+            if (first != null) {
+                first.setPrevious(null);
+            } else {
+                last = null;
+            }
+        } else if (index == count - 1) {
+            last = last.getPrevious();
+            last.setNext(null);
+        } else {
+            DoubleNode<T> current = first;
+            for (int i = 0; i < index; i++) {
+                current = current.getNext();
+            }
+
+            DoubleNode<T> previous = current.getPrevious();
+            DoubleNode<T> next = current.getNext();
+
+            previous.setNext(next);
+            next.setPrevious(previous);
+        }
+
+        count--;
+    }
 
 }
